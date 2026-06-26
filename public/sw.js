6a44d6c31352ea6454d355fe2f8c35f1b9f9cfd5
lu <lu@MacBook-Pro.local>
2026-06-26 17:27:21 +0800

    feat: PWA support + GitHub Actions deploy

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'beyond30-v1'
+const PRECACHE = [
+  '/30day-goal-journal/',
+  '/30day-goal-journal/index.html',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then(c => c.put(e.request, clone))
+        }
+        return res
+      })
+      return cached || fresh
+    })
+  )
+})
